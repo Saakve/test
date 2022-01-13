8d0145516d0e8f0e45ddcef5583cc94d9fe9ab5d
Avelino <avesanke@gmail.com>
2022-01-13 16:10:49 -0600

    SW COMPLETO

--- sw.js
+++ sw.js
@@ -16,3 +16,18 @@ self.addEventListener("install", (e) => {
         await cache.addAll(appShellFiles);
     })());
 });
+
+self.addEventListener("fetch",(e) => {
+    console.log("E:" + e);
+    e.respondWith((async () =>{
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource ${e.request.url}`);
+        if(r) return r;
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        console.log("RESPONSE: " + response);
+        return response
+    })());
+});
